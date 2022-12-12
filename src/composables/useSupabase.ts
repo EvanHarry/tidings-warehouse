abbd9c8f83b63031749ca5bfa7af5e0a1b3850d3
Evan Harry <evan.harry@adlerpelzer.com>
2022-12-12 10:31:10 +0100

    fix: session should correctly initiate when user starts the app after period of inactivity

--- src/composables/useSupabase.ts
+++ src/composables/useSupabase.ts
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'vue-router'
+
 import useAuthUser from './useAuthUser'
 
 const supabaseURL: string = import.meta.env.VITE_SUPABASE_URL
@@ -6,7 +8,13 @@ const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseURL, supabaseAnonKey)
 
-supabase.auth.onAuthStateChange(async (_, session) => {
+supabase.auth.onAuthStateChange(async (event, session) => {
+  if (event === 'TOKEN_REFRESHED') {
+    const router = useRouter()
+
+    router.push('/home')
+  }
+
   const { isAuthorized, user, userName } = useAuthUser()
 
   if (session) {
